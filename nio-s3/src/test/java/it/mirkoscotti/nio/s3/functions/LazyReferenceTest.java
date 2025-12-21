0d8af8033058da9a81fa98836defd89e5184a61b
mirko.scotti <mirko.scotti@addvalue.it>
2025-12-21 22:20:24 +0100

    LazyReference

---
+++ nio-s3/src/test/java/it/mirkoscotti/nio/s3/functions/LazyReferenceTest.java
@@ -0,0 +1,29 @@
+package it.mirkoscotti.nio.s3.functions;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ * @author mirko.scotti
+ * @version Dec 21, 2025
+ */
+@ExtendWith(MockitoExtension.class)
+class LazyReferenceTest
+{
+
+	@Test
+	void nullTest()
+	{
+		Assertions.assertThrows(NullPointerException.class, () -> LazyReference.of(null));
+	}
+
+	@Test
+	<T> void getTest(@Mock T object)
+	{
+		var reference = LazyReference.of(() -> object);
+		Assertions.assertEquals(object, reference.get());
+	}
+}
